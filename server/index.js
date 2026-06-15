fec6986aaebeb4d40722900b0b07a505b6b69a57
Eshannii <nehadoll1602@gmail.com>
2026-06-15 14:56:37 +0500

    Fix PORT fallback

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/chatbot", chatbotRoutes);
 app.use("/api/chat-history", chatHistoryRoutes);
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   connectToDatabase();
 });
